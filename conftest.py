04aed223fe54ce6b7b5f7ccefcccf17a364c265a
Anastasia <dziarkachqa@gmail.com>
2021-04-16 22:42:13 -0400

    new test homework

--- conftest.py
+++ conftest.py
@@ -1,22 +1,24 @@
 import pytest
 from selenium import webdriver
 
+
 # Как сделать чтобы можно было передавать параметры в pytest. браузер стал параметром. copy function from google.
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="Browser to run tests")
     parser.addoption("--driver_folder", action="store", default="C:\\selenium", help="Folder with stored drivers")
-    parser.addoption("--url",action="store", default="https://demo.opencart.com/", help="url to run tests" ) # to call in terminal
+    parser.addoption("--url", action="store", default="https://demo.opencart.com/",
+                     help="url to run tests")  # to call in terminal
 
 
 @pytest.fixture
 def url(request):
-    return request.config.getoption("--url") # save url to fixture
+    return request.config.getoption("--url")  # save url to fixture
+
 
 @pytest.fixture
 def browser(request):
     browser_option = request.config.getoption("--browser")
     driver_folder = request.config.getoption("--driver_folder")
-
 
     if browser_option == "chrome":
         driver = webdriver.Chrome(executable_path="{}\\chromedriver.exe".format(driver_folder))
@@ -30,36 +32,3 @@ def browser(request):
     driver.maximize_window()
     request.addfinalizer(driver.quit)
     return driver
-
-
-
-
-# И как забрать параметр. возвращаемся к примеру в гугле и копируем как забрать параметр и всталляю
-# копируем самую последнюю часть из фикстуры из того же примера на гугле
-# @pytest.fixture
-# def cmdopt(request):
-#     return request.config.getoption("--cmdopt")
-
-
-# @pytest.fixture
-# def chrome_browser(request):
-#     # создать обьект driver и зафиксировать его
-#     driver = webdriver.Chrome(executable_path="C:\\selenium\\chromedriver.exe")
-#     # добавить finalizer
-#     request.addfinalizer(driver.close)  # не ставить()  в концу ф-ции driver.close. Она вызовется только тогда
-#     # когда код дойдет до нужного места.Когда код внутри всей ф_ции выполниться он дернет метод
-#     return driver
-#
-#
-# @pytest.fixture
-# def firefox_browser(request):
-#     driver = webdriver.Firefox(executable_path="C:\\selenium\\geckodriver.exe")
-#     request.addfinalizer(driver.close)
-#     return driver
-#
-#
-# @pytest.fixture
-# def edge_browser(request):
-#     driver = webdriver.Edge(executable_path="C:\\selenium\\msedgedriver.exe")
-#     request.addfinalizer(driver.close)
-#     return driver
--- tests/test_open_cart.py
+++ tests/test_open_cart.py
@@ -1,42 +1,47 @@
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-# Test that checks availability of all links on top-bar in opencart.
-def test_top_links(browser, url):
-    browser.get(url)
-    top_bar = len(browser.find_elements_by_css_selector("#top-links li"))
-    assert top_bar == 7
-
-
-# Test that checks availability of currency button in top panel
-def test_currency_btn(browser, url):
-    browser.get(url)
-    browser.find_element_by_css_selector(".dropdown-toggle")
-
-
-# Test that checks availability of all elements on admin login panel.
-def test_admin_login(browser, url):
-    browser.get(url + "admin")
-    browser.find_element_by_css_selector("#input-username")
-    browser.find_element_by_css_selector("#input-password")
-    browser.find_element_by_link_text("Forgotten Password")
-    browser.find_element_by_css_selector("button[type='submit']")
-
-
-# Test that checks search input and cart button on main page.
+# # Test that checks search input and cart button on main page.
 def test_search_input_and_cart(browser, url):
+    explicit_wait = WebDriverWait(browser, 3)
     browser.get(url)
     search_field = browser.find_element_by_css_selector(".input-lg")
     search_button = browser.find_element_by_css_selector(".input-group-btn")
     search_field.click()
     search_field.send_keys("Iphone")
     search_button.click()
+    explicit_wait.until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, ".button-group .fa-shopping-cart")))  # жду видимости элемента
     add_cart = browser.find_element_by_css_selector(".button-group .fa-shopping-cart")
-    time.sleep(1)
     add_cart.click()
     open_cart = browser.find_element_by_css_selector(".btn-block .dropdown-toggle")
-    time.sleep(1)
     open_cart.click()
     item_in_cart = browser.find_element_by_link_text("iPhone")
     item_in_cart.click()
     assert browser.title == "iPhone"
+
+
+# Test Category Panel
+# check Categories panel transforms when screen minimized,
+# panel has the "hamburger" menu and it contains the same Categories
+# as full screen has.
+def test_category_panel(browser, url):
+    explicit_wait = WebDriverWait(browser, 3)
+    browser.get(url)
+    # check how many product Categories Category panel contains
+    product_categories = len(browser.find_elements_by_css_selector(".navbar-nav > li"))
+    assert product_categories == 8
+    browser.set_window_size(750, 820)
+    # check hamburger menu is present
+    explicit_wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".fa-bars")))
+    hamburger_menu_btn = browser.find_element_by_css_selector(".fa-bars")
+    assert hamburger_menu_btn.is_displayed() == True
+    hamburger_menu_btn.click()
+    # check product Categories list dropped down
+    product_categories_collapse = browser.find_element_by_css_selector(".navbar-collapse")
+    assert product_categories_collapse.is_displayed() == True
+    # check the word Categories is present
+    panel_name = browser.find_element_by_css_selector(".visible-xs").text
+    assert panel_name == "Categories"
